03131f4fc861ac84197b5d12ce5042329e477219
klargetfox17-code <klargetfox17@gmail.com>
2026-04-06 22:50:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "ТВОЙ_URL";
-const SUPABASE_KEY = "ТВОЙ_ANON_KEY";
+const SUPABASE_URL = "https://ddmwufcuskblflvuuixo.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbXd1ZmN1c2tibGZsdnV1aXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MDIxOTIsImV4cCI6MjA5MDk3ODE5Mn0.pKutYZa4eJ3qXkmeZrJ-VswZOxTj992lRPhdW41Un0E";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
